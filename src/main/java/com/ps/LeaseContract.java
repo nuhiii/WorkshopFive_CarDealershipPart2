61989d5a9fac697827612fb1eafeec29738d95d0
Nuhirath Rafthia <nr2793@columbia.edu>
2024-05-22 17:40:31 -0400

    Complete Phase 1 and complete LeaseContract class

--- src/main/java/com/ps/LeaseContract.java
+++ src/main/java/com/ps/LeaseContract.java
@@ -6,24 +6,16 @@ public class LeaseContract extends Contract{
     private double totalPrice;
     private double monthlyPayment;
 
+    // Constructor
     public LeaseContract(String date, String customerName, String customerEmail, Vehicle vehicleSold) {
         super(date, customerName, customerEmail, vehicleSold);
         this.expectedEndingValue = vehicleSold.getPrice() * 0.50;
         this.leaseFee = vehicleSold.getPrice() * 0.07;
     }
 
-    @Override
-    public double getTotalPrice() {
-        return 0;
-    }
-
-    @Override
-    public double getMonthlyPayment() {
-        return 0;
-    }
-
+    // Getters and Setters
     public double getExpectedEndingValue() {
-        return expectedEndingValue;
+        return  expectedEndingValue;
     }
 
     public void setExpectedEndingValue(double expectedEndingValue) {
@@ -36,5 +28,16 @@ public class LeaseContract extends Contract{
 
     public void setLeaseFee(double leaseFee) {
         this.leaseFee = leaseFee;
+    }
+
+    // Override Methods
+    @Override
+    public double getTotalPrice() {
+        return expectedEndingValue + leaseFee;
+    }
+
+    @Override
+    public double getMonthlyPayment() {
+        return (getTotalPrice() * (0.04 / 12)) / (1 - Math.pow(1 + (0.04 / 12), -36));
     }
 }
