3e4ea1d3ccceefbafbf97d6502486c75eff6d7e8
Nuhirath Rafthia <nr2793@columbia.edu>
2024-05-22 12:36:36 -0400

    Start Phase 1 and complete construction of Contract class

---
+++ src/main/java/com/ps/Contract.java
@@ -0,0 +1,55 @@
+package com.ps;
+
+public abstract class Contract {
+    private String date;
+    private String customerName;
+    private String customerEmail;
+    private String vehicleSold;
+    private double totalPrice;
+    private double monthlyPayment;
+
+    // Constructor
+    public Contract(String date, String customerName, String customerEmail, String vehicleSold) {
+        this.date = date;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicleSold = vehicleSold;
+    }
+
+    //Getters and Setters
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getVehicleSold() {
+        return vehicleSold;
+    }
+
+    public void setVehicleSold(String vehicleSold) {
+        this.vehicleSold = vehicleSold;
+    }
+
+    //Abstract Methods
+    public abstract double getTotalPrice();
+    public abstract double getMonthlyPayment();
+}
