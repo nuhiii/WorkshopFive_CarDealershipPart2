774f3e417af4322dc64dff79f4ffe3d53cf299cf
Nuhirath Rafthia <nr2793@columbia.edu>
2024-05-22 18:30:28 -0400

    Accomplish Phase 2 by implementing ContractDataManager class

---
+++ src/main/java/com/ps/ContractDataManager.java
@@ -0,0 +1,52 @@
+package com.ps;
+
+import java.io.*;
+
+public class ContractDataManager {
+
+    public void saveContract(Contract contract){
+        try {
+            BufferedWriter bufWriter = new BufferedWriter(new FileWriter("DB_Contract.csv", true));
+            if (contract instanceof SalesContract salesContract){
+                bufWriter.write(String.format("SALE|%s|%s|%s|%d|%d|%s|%s|%s|%s|%d|%.2f|%.2f|%.2f|%.2f|%.2f|%s|%.2f\n",
+                        salesContract.getCustomerName(),
+                        salesContract.getCustomerName(),
+                        salesContract.getCustomerEmail(),
+                        salesContract.getVehicleSold().getVin(),
+                        salesContract.getVehicleSold().getYear(),
+                        salesContract.getVehicleSold().getMake(),
+                        salesContract.getVehicleSold().getModel(),
+                        salesContract.getVehicleSold().getVehicleType(),
+                        salesContract.getVehicleSold().getColor(),
+                        salesContract.getVehicleSold().getOdometer(),
+                        salesContract.getVehicleSold().getPrice(),
+                        salesContract.getSalesTaxAmount(),
+                        salesContract.getRecordingFee(),
+                        salesContract.getProcessingFee(),
+                        salesContract.getTotalPrice(),
+                        salesContract.getIsFinanced() ? "YES" : "NO",
+                        salesContract.getMonthlyPayment()));
+            } else if (contract instanceof LeaseContract leaseContract){
+                bufWriter.write(String.format("LEASE|%s|%s|%s|%d|%d|%s|%s|%s|%s|%d|%.2f|%.2f|%.2f|%.2f|%.2f\n",
+                        leaseContract.getCustomerName(),
+                        leaseContract.getCustomerName(),
+                        leaseContract.getCustomerEmail(),
+                        leaseContract.getVehicleSold().getVin(),
+                        leaseContract.getVehicleSold().getYear(),
+                        leaseContract.getVehicleSold().getMake(),
+                        leaseContract.getVehicleSold().getModel(),
+                        leaseContract.getVehicleSold().getVehicleType(),
+                        leaseContract.getVehicleSold().getColor(),
+                        leaseContract.getVehicleSold().getOdometer(),
+                        leaseContract.getVehicleSold().getPrice(),
+                        leaseContract.getExpectedEndingValue(),
+                        leaseContract.getLeaseFee(),
+                        leaseContract.getTotalPrice(),
+                        leaseContract.getMonthlyPayment()));
+            }
+            bufWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
